f790314940f8a2dd2aa82b31bbd0fe436a587100
jannpf <pfeiferjann@gmail.com>
2024-07-03 16:11:53 +0200

    WIP

--- MyModel.h
+++ MyModel.h
@@ -4,6 +4,8 @@
 #include <vector>
 #include "DNest4/code/DNest4.h"
 #include "MyDistribution.h"
+#include "Eigen/Dense"
+#include "Eigen/Cholesky"
 
 class MyModel
 {
@@ -12,11 +14,18 @@ class MyModel
 
 		double background;
 		double extra_sigma; // Noise scale parameter
-		double nu; // Noise shape parameter
+
+		// Parameters for the quasi-periodic extra noise
+		double eta1, eta2, eta3, eta4;
 
 		// The signal
 		std::vector<long double> mu;
 		void calculate_mu();
+
+		// The covariance matrix for the data
+		Eigen::MatrixXd C;
+		void calculate_C();
+
 		unsigned int staleness;
 
 	public:
